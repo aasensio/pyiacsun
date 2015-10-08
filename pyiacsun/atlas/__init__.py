3212be9b3eb9ceef203a4d1c86e6df45b296f189
cdiazbas <cdiazbas@iac.es>
2015-10-08 11:59:54 +0100

    Read fts_cent.dat from pyiacsun

--- pyiacsun/atlas/__init__.py
+++ pyiacsun/atlas/__init__.py
@@ -1,1 +1,7 @@
+
+# Add fts_path to PATH
 from . import ftsread 
+import sys
+ftsdir = str(ftsread.__file__).split('/')
+strftsdir = ('/'.join(ftsdir[0:-1]))
+sys.path.insert(0, strftsdir)
--- pyiacsun/atlas/ftsread.py
+++ pyiacsun/atlas/ftsread.py
@@ -4,63 +4,64 @@
 #       /usr/pkg/rsi/idl_local/data/ftsread.pro
 
 
-def ftsread(ini,endi):
+def ftsread(ini,endi,ftsdir=None):
 
-	"""
-	Extract spectral data from the Kitt Peak FTS-Spectral-Atlas
-	as provided by H. Neckel, Hamburg.
+    """
+    Extract spectral data from the Kitt Peak FTS-Spectral-Atlas
+    as provided by H. Neckel, Hamburg.
 
-	OUTPUT: Array with stepwidth 2mA . It is based on (interpolated)  
-	Fourier-Transform-Spectra from the Kitt Peak Observatory 
-	taken by J. Brault et al.
+    OUTPUT: Array with stepwidth 2mA . It is based on (interpolated)  
+    Fourier-Transform-Spectra from the Kitt Peak Observatory 
+    taken by J. Brault et al.
 
-	CALL: [atlas,xlam] = ftsread(ini = waveIni ,endi = waveEndi)
-	with a wavelength range (3290 - 12508 A).
-	"""
+    CALL: [atlas,xlam] = ftsread(ini = waveIni ,endi = waveEndi)
+    with a wavelength range (3290 - 12508 A).
+    """
 
-	import os
-	from struct import unpack
-	import numpy as np
+    import os
+    from struct import unpack
+    import numpy as np
 
-	# Important message:
-	print('Wavelength range (3290 - 12508 A)')
-	
-	# Current directory
-	currdir = os.getcwd()
+    # Important message:
+    print('Wavelength range (3290 - 12508 A)')
+    
+    # Atlas directory
+    if ftsdir == None:
+        # sdir = '/usr/pkg/rsi/idl_local/data'
+        import sys
+        # Path from __init__.py
+        sdir = sys.path[0]
+    else:
+        sdir = ftsdir
 
-	# Atlas directory
-	sdir = '/usr/pkg/rsi/idl_local/data'
+    # Work directory
+    tdir = os.getcwd()
 
-	# Work directory
-	tdir = currdir
+    # Copy from the data a temporary range
+    skip  = (ini-3290)
+    count = abs(ini-endi)
+    os.system('dd if='+sdir+'/fts_cent.dat of='+tdir+'/tmp bs=1000 skip='+str(skip)+' count='+str(count))
+    lmbda = np.arange(ini,endi,0.002) 
+    
+    # Read the tmp file
+    FileTmp   = open('tmp', 'rb')
+    FileRead  = FileTmp.read()
+    varDecode = unpack('!'+str(len(FileRead)/2)+'h',FileRead)
+    varFinal  = np.array(varDecode)
 
-	# Copy from the data a temporary range
-	skip  = (ini-3290)
-	count = abs(ini-endi)
-	os.system('dd if='+sdir+'/fts_cent.dat of='+tdir+'/tmp bs=1000 skip='+str(skip)+' count='+str(count))
-	lmbda = np.arange(ini,endi,0.002) 
-	
-	# Read the tmp file
-	FileTmp   = open('tmp', 'rb')
-	FileRead  = FileTmp.read()
-	varDecode = unpack('!'+str(len(FileRead)/2)+'h',FileRead)
-	varFinal  = np.array(varDecode)
+    # Delete the tmp file
+    os.remove(tdir+'/tmp')
 
-	# Delete the tmp file
-	os.remove(tdir+'/tmp')
-
-	return [varFinal,lmbda]
-
-
+    return [varFinal,lmbda]
 
 if __name__ == '__main__':
-	
-	import matplotlib.pyplot as plt
-	[atlas,xlam] = ftsread(ini = 10825,endi = 10843)
+    
+    import matplotlib.pyplot as plt
+    [atlas,xlam] = ftsread(ini = 10825,endi = 10843)
 
-	plt.plot(xlam,atlas/1.E+4)
-	plt.title('Kitt Peak FTS-Spectral-Atlas')
-	plt.xlabel('Wavelength [A]')
-	plt.ylim(0.,1.)
-	plt.show()
-	
+    plt.plot(xlam,atlas/1.E+4)
+    plt.title('Kitt Peak FTS-Spectral-Atlas')
+    plt.xlabel('Wavelength [A]')
+    plt.ylim(0.,1.)
+    plt.show()
+    
