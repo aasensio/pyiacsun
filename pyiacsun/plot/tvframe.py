b3c81bc1c18413731ac39fd278407434502bf6a0
Andres Asensio Ramos <aasensioramos@gmail.com>
2016-02-13 16:41:44 +0000

    updated travis and tvframe

--- pyiacsun/plot/tvframe.py
+++ pyiacsun/plot/tvframe.py
@@ -3,13 +3,14 @@ __all__ = ['tvframe']
 import matplotlib.pyplot as pl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def tvframe(image, ax=None, bar=True, **kwargs):
+def tvframe(image, ax=None, bar=True, btitle=None, **kwargs):
 	"""Plot an image with a bar
 	
 	Args:
 	    image (float): image
 	    ax (axis): axis instance where to plot the image
 	    bar (bool, optional): detault is to plot a bar. If not, set it to False
+            btitle (string, optional): title in the bar 
 	    **kwargs: remaining arguments for the imshow
 	
 	Returns:
@@ -22,4 +23,6 @@ def tvframe(image, ax=None, bar=True, **kwargs):
 	if (bar):
 		divider = make_axes_locatable(ax)
 		cax = divider.append_axes("right", size="5%", pad=0.05)
-		pl.colorbar(im, cax=cax)
+		cbar = pl.colorbar(im, cax=cax)
+		if (btitle != None):
+			cbar.ax.set_ylabel(btitle, rotation=90)
