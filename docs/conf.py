891a06473f50fb41de9367f8ab447db804f8f656
Andres Asensio Ramos <aasensioramos@gmail.com>
2016-03-25 22:01:34 +0000

    apidoc in readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -287,3 +287,8 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:
+  os.system("sphinx-apidoc -f -o source ../pyiacsun")
